f4f13bd76151af2b28f5752ce3994db502fb3306
immprada <im.mprada@gmail.com>
2022-09-27 09:07:05 -0500

    add specs for read_line

--- spec/lib/main_spec.rb
+++ spec/lib/main_spec.rb
@@ -1,5 +1,15 @@
 require './spec/spec_helper'
 
 describe 'lib/main' do
-  
+  describe 'when read_line is called' do
+    let(:line) { ' , ,John,Smith,john.smith@mail.com' }
+
+    it 'returns a string' do
+      expect(read_line(line)).to be_a(String)
+    end
+
+    it 'returns a string with the composed name and email' do
+      expect(read_line(line)).to eq('John Smith: john.smith@mail.com')
+    end
+  end
 end
