a2ca012e3d4a0eccc72b9a21aab3ccd3d77e60c9
immprada <im.mprada@gmail.com>
2022-09-28 10:52:05 -0500

    create output html file

--- lib/template_fill.rb
+++ lib/template_fill.rb
@@ -2,5 +2,6 @@ def fill_template(template, data)
   template_filled = template.gsub('FIRST_NAME', data[:name])
           .gsub('LEGISLATORS', data[:legislators].join(', '))
 
+  File.open("output/#{data[:name]}.html", 'w+') { |file| file.write template_filled }
   puts template_filled
 end
