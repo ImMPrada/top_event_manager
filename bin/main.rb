95bd08bc5468f46d99f5cab8fcec268c57f5f6b2
immprada <im.mprada@gmail.com>
2022-09-28 10:51:55 -0500

    clear CLI at start

--- bin/main.rb
+++ bin/main.rb
@@ -5,6 +5,7 @@ require_relative '../lib/csv_solution'
 require_relative '../lib/gac_solution'
 require_relative '../lib/template_fill'
 
+system 'clear'
 path_to_file = 'event_attendees.csv'
 
 # rudimentary_extraction(path_to_file)
