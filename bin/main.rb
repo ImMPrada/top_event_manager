1641a91ab4ab22001ed8778623d23313095aeb4e
immprada <im.mprada@gmail.com>
2022-09-28 10:38:23 -0500

    implement fill_template

--- bin/main.rb
+++ bin/main.rb
@@ -3,10 +3,17 @@ require 'dotenv/load'
 require_relative '../lib/rudimentary'
 require_relative '../lib/csv_solution'
 require_relative '../lib/gac_solution'
+require_relative '../lib/template_fill'
 
 path_to_file = 'event_attendees.csv'
 
 # rudimentary_extraction(path_to_file)
 # use_csv(path_to_file)
 
-legislators_by_zipcode(path_to_file)
+data = legislators_by_zipcode(path_to_file)
+
+puts data[0]
+
+template = File.read('form-letter.html')
+
+fill_template(template, data[1])
